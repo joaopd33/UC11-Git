fe752bd46a98149085320461aa1ac54339184a04
Joao Dias <joaopd93@gmail.com>
2023-07-31 13:12:03 -0300

    feat:cadastro de produtos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,7 +12,10 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ProdutosDAO {
@@ -22,12 +25,25 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    public void cadastrarProduto(ProdutosDTO produto) {
+        conectaDAO controller = new conectaDAO();
+        conn = controller.connectDB();
+
+        try {
+            
+            prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES(?,?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            
+            prep.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Produto salvo com sucesso!:");
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Não foi possível salvar o produto. ERRO:" + erro.getMessage());
+        } finally {
+            controller.disconnectDB();         
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
@@ -39,4 +55,3 @@ public class ProdutosDAO {
     
         
 }
-
